a742116731b9e90a9fb346a8ceac463f371a3ce9
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-03 12:07:05 -0700

    image selector working

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const nameHeader = document.getElementById('name-header');
 nameInput.addEventListener('input', () => {
     nameHeader.textContent = nameInput.value;
 });
+const firstSelect = document.getElementById("first-select");
+firstSelect.addEventListener('change', () => {
+    const src = firstSelect.value;
+    chedder.src = src;
+})
